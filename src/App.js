c5d6919248a067a662497d79aa7d835635c57bde
crisa97 <crisa97@localhost.localdomain>
2022-11-14 15:03:10 -0500

    create my cv in react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header>
+      <h1>Cristhian Camilo Cruz Gonzalez</h1>
+    </header>
+  );
+}
+
+export default App;
